a489b43929373e28f420b2ab0a8f28fbdff72e8d
Victor Pontis <victor@pont.is>
2019-01-30 23:17:36 -0800

    Add expo-firebase-app package

--- android/app/src/main/java/host/exp/exponent/MainActivity.java
+++ android/app/src/main/java/host/exp/exponent/MainActivity.java
@@ -4,42 +4,47 @@ import android.os.Bundle;
 
 import com.facebook.react.ReactPackage;
 
+import java.util.Arrays;
 import java.util.List;
 
 import expo.core.interfaces.Package;
+import expo.modules.firebase.app.FirebaseAppPackage;
 import host.exp.exponent.generated.DetachBuildConstants;
 import host.exp.exponent.experience.DetachActivity;
 
 public class MainActivity extends DetachActivity {
 
-  @Override
-  public String publishedUrl() {
-    return "exp://exp.host/@scootermap/test-expokit";
-  }
+    @Override
+    public String publishedUrl() {
+        return "exp://exp.host/@scootermap/test-expokit";
+    }
 
-  @Override
-  public String developmentUrl() {
-    return DetachBuildConstants.DEVELOPMENT_URL;
-  }
+    @Override
+    public String developmentUrl() {
+        return DetachBuildConstants.DEVELOPMENT_URL;
+    }
 
-  @Override
-  public List<ReactPackage> reactPackages() {
-    return ((MainApplication) getApplication()).getPackages();
-  }
+    @Override
+    public List<ReactPackage> reactPackages() {
+        return ((MainApplication) getApplication()).getPackages();
+    }
 
-  @Override
-  public List<Package> expoPackages() {
-    return ((MainApplication) getApplication()).getExpoPackages();
-  }
+    @Override
+    public List<Package> expoPackages() {
+        // Here you can add your own packages.
+        return Arrays.<Package>asList(
+                new FirebaseAppPackage() // Include this.
+        );
+    }
 
-  @Override
-  public boolean isDebug() {
-    return BuildConfig.DEBUG;
-  }
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
+    }
 
-  @Override
-  public Bundle initialProps(Bundle expBundle) {
-    // Add extra initialProps here
-    return expBundle;
-  }
+    @Override
+    public Bundle initialProps(Bundle expBundle) {
+        // Add extra initialProps here
+        return expBundle;
+    }
 }
